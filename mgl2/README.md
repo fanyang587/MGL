04ad36f2d96839189588b9bd2104e418084e7011
Yang Fan <yang.fan@inceptioniai.org>
2021-04-12 10:10:32 +0400

    init commit

---
+++ mgl2/README.md
@@ -0,0 +1,60 @@
+# Mutual Graph Learning for Camouflaged Object Detection (CVPR2021)
+
+> Authors:
+> [Qiang Zhai](https://github.com/cvqiang/mgl), 
+> [Xin Li](https://scholar.google.com/citations?user=TK-hRO8AAAAJ&hl=en), 
+> [Fan Yang](https://scholar.google.com/citations?user=FSfSgwQAAAAJ&hl=en), 
+> [Chenglizhao Chen](https://scholar.google.com/citations?user=SGjgjBUAAAAJ&hl=zh-CN), 
+> [Hong Cheng](https://scholar.google.com/citations?user=-845MAcAAAAJ&hl=zh-CN), 
+> [Deng-Ping Fan](https://dpfan.net/).
+
+
+
+1. Configuring your environment (Prerequisites):
+    The training and testing experiments are conducted using [PyTorch](https://github.com/pytorch/pytorch).   
+    
+    Note that MGLNet is only tested on Ubuntu OS with the following environments. 
+    It may work on other operating systems as well but we do not guarantee that it will.
+    
+    + Creating a virtual environment in terminal: `conda env create -f env.yaml`.
+    
+    
+<!--2. Downloading Testing Sets: -->
+2. Downloading Testing Sets:
+    + downloading _**NEW testing dataset**_ (COD10K-test + CAMO-test + CHAMELEON), which can be found in this [Google Drive link](https://drive.google.com/file/d/1QEGnP9O7HbN_2tH999O3HRIsErIVYalx/view?usp=sharing) or [Baidu Pan link](https://pan.baidu.com/s/143yHFLAabMBT7wgXA0LrMg) with the fetch code: z83z.
+    <!--
+    + download **_NEW training dataset_** (COD10K-train) which can be found in this [Google Drive link](https://drive.google.com/file/d/1D9bf1KeeCJsxxri6d2qAC7z6O1X_fxpt/view?usp=sharing) or [Baidu Pan link](https://pan.baidu.com/s/1XL6OjpDF-MVnXOY6-bdaBg) with the fetch code:djq2.  Please refer to our original paper for other training data. -->
+    
+
+<!--3. Training Configuration:
+
+    + Assigning your customed path, like `--save_model`, `--train_img_dir`, and `--train_gt_dir` in `MyTrain.py`.
+    
+    + Just run it! -->
+
+3. Testing Configuration:
+
+    + After you download all the trained models [Google Drive link](https://drive.google.com/file/d/1KCYYcb3UM8a9Hg71f2KbowpdxGhzz6tu/view?usp=sharing) or [Baidu Pan link](https://pan.baidu.com/s/1pfFyOhOiaJVIxGjg6TFPLg) with the fetch code: ry8h, move it into './model_file/', and testing data.
+    + Assigning your comstomed path in 'config/cod_mgl50.yaml', like 'data_root', 'test_list'.
+    + Ensure consistency between 'stage' and 'model_path'. Setting 'stage: 1' and 'model_path: pre-trained/mgl_s.pth' to evaluate S-MGL model and setting 'stage: 2' and 'model_path: pre-trained/mgl_r.pth' to evaluate R-MGL model.
+    + Playing 'test.py' to generate the final prediction map, the predicted camouflaged object region and cmouflaged object edge is saved into 'exp/result' as default.
+    + You can also download the results [Google Drive link](https://drive.google.com/file/d/1Gi8JVgl3MFj3GCIW9FeE1gcmgCCzMI77/view?usp=sharing) or [Baidu Pan link](https://pan.baidu.com/s/1-0RV5ZORNznN_OVFTOoyNw) with the fetch code: b1gr.
+    
+4. Evaluation your trained model:
+
+    + One-key evaluation is written in MATLAB code (revised from [link](https://github.com/DengPingFan/CODToolbox)), 
+    please follow this the instructions in `main.m` and just run it to generate the evaluation results in 
+    `./EvaluationTool/EvaluationResults/Result-CamObjDet/`.
+
+5. If you think this work is helpful, please cite
+
+```
+@inproceedings{zhai2021Mutual,
+  title={Mutual Graph Learning for Camouflaged Object Detection},
+  author={Zhai, Qiang and Li, Xin and Yang, Fan and Chen, Chenglizhao and Cheng, Hong and Fan, Deng-Ping},
+  booktitle={Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition},
+  pages={},
+  year={2021}
+}
+```
+
---
+++ mgl2/util/util.py
@@ -0,0 +1,132 @@
+import os
+import numpy as np
+from PIL import Image
+
+import cv2
+import torch.nn.functional as F
+import torch
+from torch import nn
+import torch.nn.init as initer
+import pdb
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+#pred and target is loaded by cv2.imread with GRAYSCALE
+def calc_mae(pred, target):
+    pred = cv2.normalize(pred.astype('float'), None, 0.0, 1.0, cv2.NORM_MINMAX)
+    target = np.where(target > 127, 1.0, 0.0)
+    mae = np.mean(np.abs(pred - target))
+    return mae
+
+
+def step_learning_rate(base_lr, epoch, step_epoch, multiplier=0.1):
+    """Sets the learning rate to the base LR decayed by 10 every step epochs"""
+    lr = base_lr * (multiplier ** (epoch // step_epoch))
+    return lr
+
+
+def poly_learning_rate(base_lr, curr_iter, max_iter, power=0.9):
+    """poly learning rate policy"""
+    lr = base_lr * (1 - float(curr_iter) / max_iter) ** power
+    return lr
+
+
+def check_mkdir(dir_name):
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+
+
+def check_makedirs(dir_name):
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+
+
+def init_weights(model, conv='kaiming', batchnorm='normal', linear='kaiming', lstm='kaiming'):
+    """
+    :param model: Pytorch Model which is nn.Module
+    :param conv:  'kaiming' or 'xavier'
+    :param batchnorm: 'normal' or 'constant'
+    :param linear: 'kaiming' or 'xavier'
+    :param lstm: 'kaiming' or 'xavier'
+    """
+    for m in model.modules():
+        if isinstance(m, (nn.modules.conv._ConvNd)):
+            if conv == 'kaiming':
+                initer.kaiming_normal_(m.weight)
+            elif conv == 'xavier':
+                initer.xavier_normal_(m.weight)
+            else:
+                raise ValueError("init type of conv error.\n")
+            if m.bias is not None:
+                initer.constant_(m.bias, 0)
+
+        elif isinstance(m, (nn.modules.batchnorm._BatchNorm)):
+            if batchnorm == 'normal':
+                initer.normal_(m.weight, 1.0, 0.02)
+            elif batchnorm == 'constant':
+                initer.constant_(m.weight, 1.0)
+            else:
+                raise ValueError("init type of batchnorm error.\n")
+            initer.constant_(m.bias, 0.0)
+
+        elif isinstance(m, nn.Linear):
+            if linear == 'kaiming':
+                initer.kaiming_normal_(m.weight)
+            elif linear == 'xavier':
+                initer.xavier_normal_(m.weight)
+            else:
+                raise ValueError("init type of linear error.\n")
+            if m.bias is not None:
+                initer.constant_(m.bias, 0)
+
+        elif isinstance(m, nn.LSTM):
+            for name, param in m.named_parameters():
+                if 'weight' in name:
+                    if lstm == 'kaiming':
+                        initer.kaiming_normal_(param)
+                    elif lstm == 'xavier':
+                        initer.xavier_normal_(param)
+                    else:
+                        raise ValueError("init type of lstm error.\n")
+                elif 'bias' in name:
+                    initer.constant_(param, 0)
+
+
+def group_weight(weight_group, module, lr):
+    group_decay = []
+    group_no_decay = []
+    for m in module.modules():
+        if isinstance(m, nn.Linear):
+            group_decay.append(m.weight)
+            if m.bias is not None:
+                group_no_decay.append(m.bias)
+        elif isinstance(m, nn.modules.conv._ConvNd):
+            group_decay.append(m.weight)
+            if m.bias is not None:
+                group_no_decay.append(m.bias)
+        elif isinstance(m, nn.modules.batchnorm._BatchNorm):
+            if m.weight is not None:
+                group_no_decay.append(m.weight)
+            if m.bias is not None:
+                group_no_decay.append(m.bias)
+    assert len(list(module.parameters())) == len(group_decay) + len(group_no_decay)
+    weight_group.append(dict(params=group_decay, lr=lr))
+    weight_group.append(dict(params=group_no_decay, weight_decay=.0, lr=lr))
+    return weight_group
+
+
